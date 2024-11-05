33bbc15bbcde55581b56222b21c5fdc4349f61cb
WillYum <liamshere98@gmail.com>
2024-11-05 03:07:33 +0200

    feat: Setup multi plan

--- Assets/Patient.cs
+++ Assets/Patient.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Patient : GAgent
 {
     // Start is called before the first frame update
-    new void Start()
+    void Start()
     {
         base.Start();
         SubGoal s1 = new SubGoal("isWaiting", 1, true);
