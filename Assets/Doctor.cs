7d242eaae7807ba6898265f370f0e3c06c5c3a2c
WillYum <liamshere98@gmail.com>
2024-11-09 04:35:39 +0200

    feat: Import solution for doctor being able to do research and rest

---
+++ Assets/Doctor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doctor : GAgent
+{
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+        SubGoal s1 = new SubGoal("research", 1, false);
+        goals.Add(s1, 1);
+
+        SubGoal s2 = new SubGoal("rested", 1, false);
+        goals.Add(s2, 3);
+
+        Invoke("GetTired", Random.Range(2, 5));
+    }
+
+    void GetTired()
+    {
+        beliefs.ModifyState("exhausted", 0);
+        Invoke("GetTired", Random.Range(2, 5));
+    }
+
+}
---
+++ Assets/GOAP/GWorld.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GWorld
+{
+    private static readonly GWorld instance = new GWorld();
+    private static WorldStates world;
+    private static Queue<GameObject> patients;
+    private static Queue<GameObject> cubicles;
+    private static Queue<GameObject> offices;
+
+    static GWorld()
+    {
+        world = new WorldStates();
+        patients = new Queue<GameObject>();
+        cubicles = new Queue<GameObject>();
+        offices = new Queue<GameObject>();
+
+        GameObject[] cubes = GameObject.FindGameObjectsWithTag("Cubicle");
+        foreach (GameObject c in cubes)
+            cubicles.Enqueue(c);
+
+        if (cubes.Length > 0)
+            world.ModifyState("FreeCubicle", cubes.Length);
+
+        GameObject[] offs = GameObject.FindGameObjectsWithTag("Office");
+        foreach (GameObject o in offs)
+            offices.Enqueue(o);
+
+        if (cubes.Length > 0)
+            world.ModifyState("FreeOffice", offs.Length);
+
+        Time.timeScale = 5;
+    }
+
+    private GWorld()
+    {
+    }
+
+    public void AddPatient(GameObject p)
+    {
+        patients.Enqueue(p);
+    }
+
+    public GameObject RemovePatient()
+    {
+        if (patients.Count == 0) return null;
+        return patients.Dequeue();
+    }
+
+    public void AddCubicle(GameObject p)
+    {
+        cubicles.Enqueue(p);
+    }
+
+    public GameObject RemoveCubicle()
+    {
+        if (cubicles.Count == 0) return null;
+        return cubicles.Dequeue();
+    }
+
+    public void AddOffice(GameObject p)
+    {
+        offices.Enqueue(p);
+    }
+
+    public GameObject RemoveOffice()
+    {
+        if (offices.Count == 0) return null;
+        return offices.Dequeue();
+    }
+
+    public static GWorld Instance
+    {
+        get { return instance; }
+    }
+
+    public WorldStates GetWorld()
+    {
+        return world;
+    }
+}
