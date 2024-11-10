0c6d7bb397e0506b7205b9ec2ff08e12be87f2a4
WillYum <liamshere98@gmail.com>
2024-11-10 19:02:45 +0200

    chore: Handle code format

--- Assets/Scripts/GOAP/GAgent.cs
+++ Assets/Scripts/GOAP/GAgent.cs
@@ -2,13 +2,15 @@
 using System.Linq;
 using UnityEngine;
 
-public class SubGoal {
+public class SubGoal
+{
     // Dictionary to store our goals
     public Dictionary<string, int> sGoals;
     // Bool to store if goal should be removed after it has been achieved
     public bool remove;
     // Constructor
-    public SubGoal(string s, int i, bool r) {
+    public SubGoal(string s, int i, bool r)
+    {
 
         sGoals = new Dictionary<string, int>();
         sGoals.Add(s, i);
@@ -16,7 +18,8 @@ public class SubGoal {
     }
 }
 
-public class GAgent : MonoBehaviour {
+public class GAgent : MonoBehaviour
+{
     // Store our list of actions
     public List<GAction> actions = new List<GAction>();
     // Dictionary of subgoals
@@ -39,7 +42,8 @@ public class GAgent : MonoBehaviour {
     Vector3 destination = Vector3.zero;
 
     // Start is called before the first frame update
-    public void Start() {
+    public void Start()
+    {
         GAction[] acts = this.GetComponents<GAction>();
         foreach (GAction a in acts)
             actions.Add(a);
@@ -49,15 +53,18 @@ public class GAgent : MonoBehaviour {
     bool invoked = false;
     //an invoked method to allow an agent to be performing a task
     //for a set location
-    void CompleteAction() {
+    void CompleteAction()
+    {
         currentAction.running = false;
         currentAction.PostPerform();
         invoked = false;
     }
 
-    void LateUpdate() {
+    void LateUpdate()
+    {
         //if there's a current action and it is still running
-        if (currentAction != null && currentAction.running) {
+        if (currentAction != null && currentAction.running)
+        {
             // Find the distance to the target
             float distanceToTarget = Vector3.Distance(destination, this.transform.position);
             //Debug.Log(currentAction.agent.hasPath + "   " + distanceToTarget);
@@ -65,7 +72,8 @@ public class GAgent : MonoBehaviour {
             if (distanceToTarget < 2f)//currentAction.agent.remainingDistance < 0.5f)
             {
                 // Debug.Log("Distance to Goal: " + currentAction.agent.remainingDistance);
-                if (!invoked) {
+                if (!invoked)
+                {
                     //if the action movement is complete wait
                     //a certain duration for it to be completed
                     Invoke("CompleteAction", currentAction.duration);
@@ -76,16 +84,19 @@ public class GAgent : MonoBehaviour {
         }
 
         // Check we have a planner and an actionQueue
-        if (planner == null || actionQueue == null) {
+        if (planner == null || actionQueue == null)
+        {
             planner = new GPlanner();
 
             // Sort the goals in descending order and store them in sortedGoals
             var sortedGoals = from entry in goals orderby entry.Value descending select entry;
             //look through each goal to find one that has an achievable plan
-            foreach (KeyValuePair<SubGoal, int> sg in sortedGoals) {
+            foreach (KeyValuePair<SubGoal, int> sg in sortedGoals)
+            {
                 actionQueue = planner.plan(actions, sg.Key.sGoals, beliefs);
                 // If actionQueue is not = null then we must have a plan
-                if (actionQueue != null) {
+                if (actionQueue != null)
+                {
                     // Set the current goal
                     currentGoal = sg.Key;
                     break;
@@ -94,9 +105,11 @@ public class GAgent : MonoBehaviour {
         }
 
         // Have we an actionQueue
-        if (actionQueue != null && actionQueue.Count == 0) {
+        if (actionQueue != null && actionQueue.Count == 0)
+        {
             // Check if currentGoal is removable
-            if (currentGoal.remove) {
+            if (currentGoal.remove)
+            {
                 // Remove it
                 goals.Remove(currentGoal);
             }
@@ -105,16 +118,19 @@ public class GAgent : MonoBehaviour {
         }
 
         // Do we still have actions
-        if (actionQueue != null && actionQueue.Count > 0) {
+        if (actionQueue != null && actionQueue.Count > 0)
+        {
             // Remove the top action of the queue and put it in currentAction
             currentAction = actionQueue.Dequeue();
-            if (currentAction.PrePerform()) {
+            if (currentAction.PrePerform())
+            {
                 // Get our current object
                 if (currentAction.target == null && currentAction.targetTag != "")
                     // Activate the current action
                     currentAction.target = GameObject.FindWithTag(currentAction.targetTag);
 
-                if (currentAction.target != null) {
+                if (currentAction.target != null)
+                {
                     // Activate the current action
                     currentAction.running = true;
                     // Pass in the office then look for its cube
@@ -127,7 +143,9 @@ public class GAgent : MonoBehaviour {
                     // Pass Unities AI the destination for the agent
                     currentAction.agent.SetDestination(destination);
                 }
-            } else {
+            }
+            else
+            {
                 // Force a new plan
                 actionQueue = null;
             }
